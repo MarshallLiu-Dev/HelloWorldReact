14ac3440e728c5a072ddff96dae0ab2f751507ee
Paulo Henrique M de Morais <111690624+MarshallLiu-Dev@users.noreply.github.com>
2023-04-04 11:31:52 -0300

    first commit

---
+++ react/hello-wold/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Hello World! React </h1>
+
+      <p>Olá mundinho react br! Cheguei para causar e mostrar quem sou . <br></br> Não me importo com os haters que dizem que sou difícil de aprender ou que tenho muitas dependências.<br></br>  Eles são só invejosos que não conseguem acompanhar a minha evolução. Eu sou o futuro da web e ninguém vai me parar.</p>
+
+    </div>
+  );
+}
+
+export default App;
